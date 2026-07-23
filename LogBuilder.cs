f88918745ef5450d2152f95847efaf29b2eb28e8
Arda Eren <Poedx2@gmail.com>
2026-07-23 07:49:17 +0300

    init

---
+++ LogBuilder.cs
@@ -0,0 +1,194 @@
+﻿using System.Text;
+
+namespace ColoredLogger
+{
+    public class LogBuilder
+    {
+        public LogBuilder() { }
+
+        private StringBuilder logBuilderSB = new();
+
+        public LogBuilder Write(object text)
+        {
+            logBuilderSB.Append(text.ToString() ?? "");
+            return this;
+        }
+        public LogBuilder WriteLine(object text)
+        {
+            logBuilderSB.AppendLine(text.ToString() ?? "");
+            return this;
+        }
+
+        public LogBuilder DefaultFGFromLogLevel(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Info:
+                    this.defaultFGColor = LogColors.brightBlueRGB;
+                    break;
+                case LogLevel.Detail:
+                    this.defaultFGColor = LogColors.brightBlackRGB;
+                    break;
+                case LogLevel.Warning:
+                    this.defaultFGColor = LogColors.brightYellowRGB;
+                    break;
+                case LogLevel.Error:
+                    this.defaultFGColor = LogColors.brightRedRGB;
+                    break;
+            }
+            return this;
+        }
+
+        public (int r, int g, int b) defaultFGColor = LogColors.whiteRGB;
+        public (int r, int g, int b) defaultBGColor = LogColors.blackRGB;
+
+        public LogBuilder NewLine() => WriteLine("");
+        public LogBuilder NL() => WriteLine("");
+
+        #region Foreground Colors
+        public LogBuilder Default()
+        { (int r, int g, int b) rgb = this.defaultFGColor; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+
+        public LogBuilder Color(int r, int g, int b)
+            => Write(LogColors.GetANSICodeFG(r, g, b));
+
+        public LogBuilder Black()
+        { (int r, int g, int b) rgb = LogColors.blackRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder Red()
+        { (int r, int g, int b) rgb = LogColors.redRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder Green()
+        { (int r, int g, int b) rgb = LogColors.greenRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder Yellow()
+        { (int r, int g, int b) rgb = LogColors.yellowRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder Blue()
+        { (int r, int g, int b) rgb = LogColors.blueRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder Magenta()
+        { (int r, int g, int b) rgb = LogColors.magentaRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder Cyan()
+        { (int r, int g, int b) rgb = LogColors.cyanRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder White()
+        { (int r, int g, int b) rgb = LogColors.whiteRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+
+        public LogBuilder BrightBlack()
+        { (int r, int g, int b) rgb = LogColors.brightBlackRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightRed()
+        { (int r, int g, int b) rgb = LogColors.brightRedRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightGreen()
+        { (int r, int g, int b) rgb = LogColors.brightGreenRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightYellow()
+        { (int r, int g, int b) rgb = LogColors.brightYellowRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightBlue()
+        { (int r, int g, int b) rgb = LogColors.brightBlueRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightMagenta()
+        { (int r, int g, int b) rgb = LogColors.brightMagentaRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightCyan()
+        { (int r, int g, int b) rgb = LogColors.brightCyanRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BrightWhite()
+        { (int r, int g, int b) rgb = LogColors.brightWhiteRGB; return Write(LogColors.GetANSICodeFG(rgb.r, rgb.g, rgb.b)); }
+
+        #region Foreground Aliases
+        public LogBuilder Col(int r, int g, int b)
+            => Color(r, g, b);
+
+
+        public LogBuilder D() => Default();
+        public LogBuilder K() => Black();
+        public LogBuilder R() => Red();
+        public LogBuilder G() => Green();
+        public LogBuilder Y() => Yellow();
+        public LogBuilder B() => Blue();
+        public LogBuilder M() => Magenta();
+        public LogBuilder C() => Cyan();
+        public LogBuilder W() => White();
+
+        public LogBuilder BK() => BrightBlack();
+        public LogBuilder BR() => BrightRed();
+        public LogBuilder BG() => BrightGreen();
+        public LogBuilder BY() => BrightYellow();
+        public LogBuilder BB() => BrightBlue();
+        public LogBuilder BM() => BrightMagenta();
+        public LogBuilder BC() => BrightCyan();
+        public LogBuilder BW() => BrightWhite();
+        #endregion
+        #endregion
+
+        #region Background Colors
+        public LogBuilder BGColor(int r, int g, int b)
+            => Write(LogColors.GetANSICodeBG(r, g, b));
+
+        public LogBuilder BGDefault()
+        { (int r, int g, int b) rgb = this.defaultFGColor; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+
+        public LogBuilder BGBlack()
+        { (int r, int g, int b) rgb = LogColors.blackRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGRed()
+        { (int r, int g, int b) rgb = LogColors.redRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGGreen()
+        { (int r, int g, int b) rgb = LogColors.greenRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGYellow()
+        { (int r, int g, int b) rgb = LogColors.yellowRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBlue()
+        { (int r, int g, int b) rgb = LogColors.blueRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGMagenta()
+        { (int r, int g, int b) rgb = LogColors.magentaRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGCyan()
+        { (int r, int g, int b) rgb = LogColors.cyanRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGWhite()
+        { (int r, int g, int b) rgb = LogColors.whiteRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+
+        public LogBuilder BGBrightBlack()
+        { (int r, int g, int b) rgb = LogColors.brightBlackRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightRed()
+        { (int r, int g, int b) rgb = LogColors.brightRedRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightGreen()
+        { (int r, int g, int b) rgb = LogColors.brightGreenRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightYellow()
+        { (int r, int g, int b) rgb = LogColors.brightYellowRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightBlue()
+        { (int r, int g, int b) rgb = LogColors.brightBlueRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightMagenta()
+        { (int r, int g, int b) rgb = LogColors.brightMagentaRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightCyan()
+        { (int r, int g, int b) rgb = LogColors.brightCyanRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+        public LogBuilder BGBrightWhite()
+        { (int r, int g, int b) rgb = LogColors.brightWhiteRGB; return Write(LogColors.GetANSICodeBG(rgb.r, rgb.g, rgb.b)); }
+
+        #region Background Aliases
+        public LogBuilder BGCol(int r, int g, int b)
+            => BGColor(r, g, b);
+
+        public LogBuilder BGD() => BGDefault();
+        public LogBuilder BGK() => BGBlack();
+        public LogBuilder BGR() => BGRed();
+        public LogBuilder BGG() => BGGreen();
+        public LogBuilder BGY() => BGYellow();
+        public LogBuilder BGB() => BGBlue();
+        public LogBuilder BGM() => BGMagenta();
+        public LogBuilder BGC() => BGCyan();
+        public LogBuilder BGW() => BGWhite();
+
+        public LogBuilder BGBK() => BGBrightBlack();
+        public LogBuilder BGBR() => BGBrightRed();
+        public LogBuilder BGBG() => BGBrightGreen();
+        public LogBuilder BGBY() => BGBrightYellow();
+        public LogBuilder BGBB() => BGBrightBlue();
+        public LogBuilder BGBM() => BGBrightMagenta();
+        public LogBuilder BGBC() => BGBrightCyan();
+        public LogBuilder BGBW() => BGBrightWhite();
+        #endregion
+        #endregion
+
+        public override string ToString()
+        {
+            string res = logBuilderSB.ToString();
+            return res;
+        }
+
+        public void Log()
+        {
+            Logger.Log(this.ToString());
+        }
+
+
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,331 @@
+﻿using System.Diagnostics;
+using Wacton.Unicolour;
+
+namespace ColoredLogger
+{
+    public class Logger
+    {
+        public static void Log(string info, LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Info:
+                    LogColors.DefaultForeground = LogColors.brightBlueRGB;
+                    Log(LogColors.BrightBlue(info));
+                    break;
+                case LogLevel.Detail:
+                    LogColors.DefaultForeground = LogColors.brightBlackRGB;
+                    Log(LogColors.BrightBlack(info));
+                    break;
+                case LogLevel.Warning:
+                    LogColors.DefaultForeground = LogColors.brightYellowRGB;
+                    Log(LogColors.BrightYellow(info));
+                    break;
+                case LogLevel.Error:
+                    LogColors.DefaultForeground = LogColors.brightRedRGB;
+                    Log(LogColors.BrightRed(info));
+                    break;
+                default:
+                    Log(LogColors.BrightWhite(info));
+                    break;
+            }
+        }
+
+        public static void LogRaw(string text) => Console.WriteLine(text);
+        public static void Log(string info)
+        {
+
+            LogReal(info);
+            // since we change the color when we log, logging without us changing color means the app crashed, so we should set the color to red
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        private static void LogReal(string info)
+        {
+            // replace return to normals with the current color so we can change the color of texts
+            string text = Indent + info;
+            Console.WriteLine(text);
+        }
+
+        private static readonly Stopwatch appTimer = Stopwatch.StartNew();
+
+        private static Stack<double> startTimes = new Stack<double>();
+
+        public static void BeginTimingBlock()
+        {
+            startTimes.Push(appTimer.Elapsed.TotalSeconds);
+        }
+
+        public static double EndTimingBlock()
+        {
+            if (startTimes.Count == 0) return 0.0;
+                
+            return appTimer.Elapsed.TotalSeconds - startTimes.Pop();
+        }
+        public static string EndTimingBlockFormatted()
+        {
+            return $"{EndTimingBlock():F3}ms";
+        }
+        private const int IndentLenght = 2;
+        private static int IndentLevel = 0;
+        private static string Indent = "";
+        public static void PushIndentLevel()
+        {
+            IndentLevel++;
+            Indent = new(' ', IndentLevel * IndentLenght);
+        }
+        public static void PopIndentLevel()
+        {
+            IndentLevel--;
+            Indent = new(' ', IndentLevel * IndentLenght);
+        }
+
+        private static Stack<long> startMemoryUsages = new Stack<long>();
+        private static long CurrMemory => GC.GetTotalMemory(forceFullCollection: false);
+        public static void BeginMemoryBlock()
+        {
+            startMemoryUsages.Push(CurrMemory);
+        }
+        public static long EndMemoryBlock()
+        {
+            return CurrMemory - startMemoryUsages.Pop();
+        }
+        public static string EndMemoryBlockFormatted()
+        {
+            long delta = EndMemoryBlock();
+            return FormatBytes(delta) + " RAM";
+        }
+        public static string FormatBytes(long bytes)
+        {
+            bool isNegative = (bytes < 0);
+            bytes = Math.Abs(bytes);
+            const long KB = 1024;
+            const long MB = 1024 * KB;
+            const long GB = 1024 * MB;
+
+            string negative = "";
+            if (isNegative)
+                negative = "-";
+
+            if (bytes >= GB) return $"{negative}{bytes / (double)GB:F2} GB";
+            if (bytes >= MB) return $"{negative}{bytes / (double)MB:F2} MB";
+            if (bytes >= KB) return $"{negative}{bytes / (double)KB:F2} KB";
+
+            return $"{negative}{bytes} B";
+        }
+
+        public static void PrintEmptyLine() => Console.WriteLine();
+   
+        public static void PrintTestColors()
+        {
+            var lb = new LogBuilder();
+
+            // print all default foreground colors
+            lb
+            .K().BGW().Write("Black").BGK().Write(" ")
+            .R().Write("Red ")
+            .G().Write("Green ")
+            .Y().Write("Yellow ")
+            .B().Write("Blue ")
+            .M().Write("Magenta ")
+            .C().Write("Cyan ")
+            .W().WriteLine("White");
+
+            lb
+            .BK().Write("BrightBlack ")
+            .BR().Write("BrightRed ")
+            .BG().Write("BrightGreen ")
+            .BY().Write("BrightYellow ")
+            .BB().Write("BrightBlue ")
+            .BM().Write("BrightMagenta ")
+            .BC().Write("BrightCyan ")
+            .BW().WriteLine("BrightWhite");
+
+            lb.Log();
+
+            // Print 32 squares with rainbow colors
+            Console.WriteLine();
+            var rainbowColors = new (int r, int g, int b)[]
+            {
+                (255,0,0), (255,127,0), (255,255,0), (127,255,0),
+                (0,255,0), (0,255,127), (0,255,255), (0,127,255),
+                (0,0,255), (127,0,255), (255,0,255), (255,0,127),
+                (255,64,0), (255,191,0), (191,255,0), (64,255,0),
+                (0,255,64), (0,255,191), (0,191,255), (0,64,255),
+                (64,0,255), (191,0,255), (255,0,191), (255,0,64),
+                (128,0,128), (128,0,255), (0,128,128), (0,128,255),
+                (128,128,0), (255,128,0), (128,0,0), (0,128,0)
+            };
+
+            int n = 64;
+            var lbSquares = new LogBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                float hue = i / (float)n; // 0.0 to 1.0
+                Unicolour col = new Unicolour(ColourSpace.Hsb, hue, 1, 1);
+                Rgb rgb = col.Rgb;
+                lbSquares.Col(
+                    (int)(rgb.R * 255),
+                    (int)(rgb.G * 255),
+                    (int)(rgb.B * 255)
+                    ).Write("■");
+            }
+
+
+            lbSquares.WriteLine(""); // New line at the end
+            lbSquares.Log();
+        }
+    }
+
+    public enum LogLevel
+    {
+        Info,
+        Detail,
+        Warning,
+        Error,
+    }
+
+    public class LogColors
+    {
+        // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+        public static string Rgb(object source, int? fr = null, int? fg = null, int? fb = null, int? br = null, int? bg = null, int? bb = null)
+        {
+            string text = source?.ToString() ?? "";
+            string ansiCode = "";
+            if (((int?[])[fr, fg, fb]).Any(v => v.HasValue))
+                ansiCode = GetANSICodeFG((int)fr!, (int)fg!, (int)fb!);
+
+            if (((int?[])[br, bg, bb]).Any(v => v.HasValue))
+                ansiCode += GetANSICodeBG((int)br!, (int)bg!, (int)bb!);
+
+            return $"{ansiCode}{text}{CURR_DEFAULT}";
+        }
+
+        public static string GetANSICodeFG(int fr, int fg, int fb)
+        {
+            string fgCode = $"\u001b[38;2;{fr};{fg};{fb}m";
+            return fgCode;
+        }
+        public static string GetANSICodeBG(int br, int bg, int bb)
+        {
+            string bgCode = $"\u001b[48;2;{br};{bg};{bb}m";
+            return bgCode;
+        }
+
+        public static string GetANSICode(int fr, int fg, int fb, int br, int bg, int bb)
+            => GetANSICodeFG(fr, fg, fb) + GetANSICodeBG(br, bg, bb);
+
+        public static (int r, int g, int b) DefaultForeground
+        {
+            get; set
+            {
+                field = value;
+                CURR_DEFAULT = $"\u001b[38;2;{value.r};{value.g};{value.b}m";
+            }
+        }
+        public static string CURR_DEFAULT = "\u001b[0m";
+        public static string RESET = "\u001b[0m";
+
+        #region Pre Colors
+        public static (int r, int g, int b) blackRGB = (0, 0, 0);
+        public static (int r, int g, int b) redRGB = (255, 0, 0);
+        public static (int r, int g, int b) greenRGB = (0, 255, 0);
+        public static (int r, int g, int b) yellowRGB = (255, 255, 0);
+        public static (int r, int g, int b) blueRGB = (0, 0, 255);
+        public static (int r, int g, int b) magentaRGB = (255, 0, 255);
+        public static (int r, int g, int b) cyanRGB = (0, 255, 255);
+        public static (int r, int g, int b) whiteRGB = (255, 255, 255);
+
+        public static (int r, int g, int b) brightBlackRGB = (85, 85, 85);
+        public static (int r, int g, int b) brightRedRGB = (255, 85, 85);
+        public static (int r, int g, int b) brightGreenRGB = (85, 255, 85);
+        public static (int r, int g, int b) brightYellowRGB = (255, 255, 85);
+        public static (int r, int g, int b) brightBlueRGB = (85, 85, 255);
+        public static (int r, int g, int b) brightMagentaRGB = (255, 85, 255);
+        public static (int r, int g, int b) brightCyanRGB = (85, 255, 255);
+        public static (int r, int g, int b) brightWhiteRGB = (255, 255, 255);
+        #endregion
+
+        #region Colors
+        public static string Black(object text) => Rgb(text, blackRGB.r, blackRGB.g, blackRGB.b);
+        public static string Red(object text) => Rgb(text, redRGB.r, redRGB.g, redRGB.b);
+        public static string Green(object text) => Rgb(text, greenRGB.r, greenRGB.g, greenRGB.b);
+        public static string Yellow(object text) => Rgb(text, yellowRGB.r, yellowRGB.g, yellowRGB.b);
+        public static string Blue(object text) => Rgb(text, blueRGB.r, blueRGB.g, blueRGB.b);
+        public static string Magenta(object text) => Rgb(text, magentaRGB.r, magentaRGB.g, magentaRGB.b);
+        public static string Cyan(object text) => Rgb(text, cyanRGB.r, cyanRGB.g, cyanRGB.b);
+        public static string White(object text) => Rgb(text, whiteRGB.r, whiteRGB.g, whiteRGB.b);
+
+        public static string BrightBlack(object text) => Rgb(text, brightBlackRGB.r, brightBlackRGB.g, brightBlackRGB.b);
+        public static string BrightRed(object text) => Rgb(text, brightRedRGB.r, brightRedRGB.g, brightRedRGB.b);
+        public static string BrightGreen(object text) => Rgb(text, brightGreenRGB.r, brightGreenRGB.g, brightGreenRGB.b);
+        public static string BrightYellow(object text) => Rgb(text, brightYellowRGB.r, brightYellowRGB.g, brightYellowRGB.b);
+        public static string BrightBlue(object text) => Rgb(text, brightBlueRGB.r, brightBlueRGB.g, brightBlueRGB.b);
+        public static string BrightMagenta(object text) => Rgb(text, brightMagentaRGB.r, brightMagentaRGB.g, brightMagentaRGB.b);
+        public static string BrightCyan(object text) => Rgb(text, brightCyanRGB.r, brightCyanRGB.g, brightCyanRGB.b);
+        public static string BrightWhite(object text) => Rgb(text, brightWhiteRGB.r, brightWhiteRGB.g, brightWhiteRGB.b);
+        #region Aliases
+        public static string K(object text) => Black(text);
+        public static string R(object text) => Red(text);
+        public static string G(object text) => Green(text);
+        public static string Y(object text) => Yellow(text);
+        public static string B(object text) => Blue(text);
+        public static string M(object text) => Magenta(text);
+        public static string C(object text) => Cyan(text);
+        public static string W(object text) => White(text);
+
+        public static string BK(object text) => BrightBlack(text);
+        public static string BR(object text) => BrightRed(text);
+        public static string BG(object text) => BrightGreen(text);
+        public static string BY(object text) => BrightYellow(text);
+        public static string BB(object text) => BrightBlue(text);
+        public static string BM(object text) => BrightMagenta(text);
+        public static string BC(object text) => BrightCyan(text);
+        public static string BW(object text) => BrightWhite(text);
+        #endregion
+        #endregion
+
+        #region Background Colors
+        public static string BGBlack(object text) => Rgb(text, br: blackRGB.r, bg: blackRGB.g, bb: blackRGB.b);
+        public static string BGRed(object text) => Rgb(text, br: redRGB.r, bg: redRGB.g, bb: redRGB.b);
+        public static string BGGreen(object text) => Rgb(text, br: greenRGB.r, bg: greenRGB.g, bb: greenRGB.b);
+        public static string BGYellow(object text) => Rgb(text, br: yellowRGB.r, bg: yellowRGB.g, bb: yellowRGB.b);
+        public static string BGBlue(object text) => Rgb(text, br: blueRGB.r, bg: blueRGB.g, bb: blueRGB.b);
+        public static string BGMagenta(object text) => Rgb(text, br: magentaRGB.r, bg: magentaRGB.g, bb: magentaRGB.b);
+        public static string BGCyan(object text) => Rgb(text, br: cyanRGB.r, bg: cyanRGB.g, bb: cyanRGB.b);
+        public static string BGWhite(object text) => Rgb(text, br: whiteRGB.r, bg: whiteRGB.g, bb: whiteRGB.b);
+
+        public static string BGBrightBlack(object text) => Rgb(text, br: brightBlackRGB.r, bg: brightBlackRGB.g, bb: brightBlackRGB.b);
+        public static string BGBrightRed(object text) => Rgb(text, br: brightRedRGB.r, bg: brightRedRGB.g, bb: brightRedRGB.b);
+        public static string BGBrightGreen(object text) => Rgb(text, br: brightGreenRGB.r, bg: brightGreenRGB.g, bb: brightGreenRGB.b);
+        public static string BGBrightYellow(object text) => Rgb(text, br: brightYellowRGB.r, bg: brightYellowRGB.g, bb: brightYellowRGB.b);
+        public static string BGBrightBlue(object text) => Rgb(text, br: brightBlueRGB.r, bg: brightBlueRGB.g, bb: brightBlueRGB.b);
+        public static string BGBrightMagenta(object text) => Rgb(text, br: brightMagentaRGB.r, bg: brightMagentaRGB.g, bb: brightMagentaRGB.b);
+        public static string BGBrightCyan(object text) => Rgb(text, br: brightCyanRGB.r, bg: brightCyanRGB.g, bb: brightCyanRGB.b);
+        public static string BGBrightWhite(object text) => Rgb(text, br: brightWhiteRGB.r, bg: brightWhiteRGB.g, bb: brightWhiteRGB.b);
+
+
+        #region Background Aliases
+        public static string BGK(object text) => BGBlack(text);
+        public static string BGR(object text) => BGRed(text);
+        public static string BGG(object text) => BGGreen(text);
+        public static string BGY(object text) => BGYellow(text);
+        public static string BGB(object text) => BGBlue(text);
+        public static string BGM(object text) => BGMagenta(text);
+        public static string BGC(object text) => BGCyan(text);
+        public static string BGW(object text) => BGWhite(text);
+
+        public static string BGBK(object text) => BGBrightBlack(text);
+        public static string BGBR(object text) => BGBrightRed(text);
+        public static string BGBG(object text) => BGBrightGreen(text);
+        public static string BGBY(object text) => BGBrightYellow(text);
+        public static string BGBB(object text) => BGBrightBlue(text);
+        public static string BGBM(object text) => BGBrightMagenta(text);
+        public static string BGBC(object text) => BGBrightCyan(text);
+        public static string BGBW(object text) => BGBrightWhite(text);
+        #endregion
+        #endregion
+
+    }
+}
